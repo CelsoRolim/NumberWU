5e79ef46a06be1a170727ba78e435f25a1d4a10f
Celso <cgr@email.com>
2016-06-18 00:18:43 -0400

    Issue:  Added script to load level for Start button. 	Added script to quit game for Quit button. Developer: Celso

---
+++ Assets/Scripts/EmptyLevelMangerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Empty object used to attach the method to a button onclick event.
+
+public class EmptyLevelMangerScript : MonoBehaviour {
+
+	// method to load new level
+	public void loadLevel(string newLevel) {
+		Debug.Log ("Level (" + newLevel + ") will be loaded.");
+	}
+
+	// method to process the quit request
+	public void quitRequestButton() {
+		Debug.Log ("Quit game requested. Quiting game...");
+	}
+}
