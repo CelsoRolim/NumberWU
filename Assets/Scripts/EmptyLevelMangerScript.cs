a88fcc841dc9429017c9e63607c09f2a2c9c14dc
Celso <cgr@email.com>
2016-06-18 21:14:27 -0400

    Issue: Added code to load scenes. Start scene to Game scene. Quit scene. Developer: Celso

--- Assets/Scripts/EmptyLevelMangerScript.cs
+++ Assets/Scripts/EmptyLevelMangerScript.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 // Empty object used to attach the method to a button onclick event.
 
 public class EmptyLevelMangerScript : MonoBehaviour {
 
+	// OBS: scene and level are the same.
+
 	// method to load new level
 	public void loadLevel(string newLevel) {
 		Debug.Log ("Level (" + newLevel + ") will be loaded.");
+
+		// load level "newLevel"
+		SceneManager.LoadScene(newLevel);
 	}
 
 	// method to process the quit request
 	public void quitRequestButton() {
 		Debug.Log ("Quit game requested. Quiting game...");
+
+		// Quit game
+		// Do not use for mobile projects. It is a bad pattern.
+		Application.Quit();
 	}
 }
